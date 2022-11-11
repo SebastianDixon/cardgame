239586dfed8e931e8584d19d06d081b7b65824b0
SebastianDixon <smedixon@gmail.com>
2022-11-11 16:42:53 +0000

    many updates

--- src/main/Player.java
+++ src/main/Player.java
@@ -1,13 +1,32 @@
 package main;
 
+import java.io.File;
 import java.util.ArrayList;
 
-public class Player {
-    private final int playerId;
-    private final ArrayList<Integer> cards = new ArrayList<>();
+public class Player extends Thread{
+    private int playerId;
+    private ArrayList<Integer> cards = new ArrayList<>();
+    private volatile boolean gameOver = false;
+    private File player_file;
 
     public Player() {
         playerId = PlayerGenerator.getId();
+        player_file = new File("game_files/player" + playerId + "_output.txt");
+    }
+
+    public void run() {
+        logPlayer("Player " + this.playerId + " has joined the game");
+        logPlayer("player " + this.playerId + " initial hand is " + this);
+        boolean won = checkWon();
+    }
+
+    public boolean checkWon() {
+        for (int i: cards) {
+            if (i != this.getPlayerId()) {
+                return false;
+            }
+        }
+        return true;
     }
 
     public void addCard(int n) { cards.add(n); }
@@ -34,5 +53,10 @@ public class Player {
             cards = cards + i + " ";
         }
         return cards;
+    }
+
+    private void logPlayer(String s) {
+        System.out.println(s);
+        Logger.writeNewLine(player_file, s);
     }
 }
